3e090fcaf4df9a2182307306690d995ecbd84cd7
Ahmad Faisal <ahmadfaisall.020597@gmail.com>
2025-12-17 01:13:20 +0700

    fixing

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Iklan;
-use App\Models\Kalangan;
 use App\Models\PesertaPendaftaran;
 use Illuminate\Support\Facades\DB;
 
@@ -11,10 +10,6 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        // =====================
-        // TOTAL GLOBAL
-        // =====================
-
         $totalBiayaIklan = Iklan::sum('biaya');
         $totalPendapatan = PesertaPendaftaran::sum('biaya_pendaftaran');
         $keuntungan = $totalPendapatan - $totalBiayaIklan;
@@ -23,11 +18,7 @@ class DashboardController extends Controller
             ? ($keuntungan / $totalBiayaIklan) * 100
             : 0;
 
-        // =====================
-        // TOTAL PESERTA PER KALANGAN
-        // =====================
-
-        $kalanganStats = PesertaPendaftaran::select(
+        $kalangan = PesertaPendaftaran::select(
             'kalangan_id',
             'nama_kalangan',
             DB::raw('COUNT(*) as total_peserta')
@@ -35,10 +26,12 @@ class DashboardController extends Controller
             ->groupBy('kalangan_id', 'nama_kalangan')
             ->get();
 
-        // =====================
-        // TOTAL PESERTA PER IKLAN
-        // =====================
-        $iklanStats = PesertaPendaftaran::join('iklans', 'peserta_pendaftarans.iklan_id', '=', 'iklans.id')
+        $iklan = PesertaPendaftaran::join(
+            'iklans',
+            'peserta_pendaftarans.iklan_id',
+            '=',
+            'iklans.id'
+        )
             ->select(
                 'iklans.id as iklan_id',
                 'iklans.nama_iklan',
@@ -47,9 +40,29 @@ class DashboardController extends Controller
                 DB::raw('COUNT(peserta_pendaftarans.id) as total_peserta'),
                 DB::raw('SUM(peserta_pendaftarans.biaya_pendaftaran) as total_pendapatan'),
                 DB::raw('COUNT(peserta_pendaftarans.id) * iklans.biaya as total_biaya_iklan'),
-                DB::raw('SUM(peserta_pendaftarans.biaya_pendaftaran) - (COUNT(peserta_pendaftarans.id) * iklans.biaya) as keuntungan')
+                DB::raw('SUM(peserta_pendaftarans.biaya_pendaftaran) -
+                 (COUNT(peserta_pendaftarans.id) * iklans.biaya) as keuntungan')
             )
             ->groupBy('iklans.id', 'iklans.nama_iklan', 'iklans.biaya')
+            ->orderByDesc('total_peserta')
+            ->get();
+
+        $webinar = PesertaPendaftaran::select(
+            'webinar_id',
+            'nama_webinar',
+            DB::raw('COUNT(*) as total_peserta')
+        )
+            ->groupBy('webinar_id', 'nama_webinar')
+            ->orderByDesc('total_peserta')
+            ->limit(10)
+            ->get();
+
+        $kalanganTerbanyak = PesertaPendaftaran::select(
+            'kalangan_id',
+            'nama_kalangan',
+            DB::raw('COUNT(*) as total_peserta')
+        )
+            ->groupBy('kalangan_id', 'nama_kalangan')
             ->orderByDesc('total_peserta')
             ->limit(10)
             ->get();
@@ -59,10 +72,13 @@ class DashboardController extends Controller
                 'total_biaya_iklan' => $totalBiayaIklan,
                 'total_pendapatan' => $totalPendapatan,
                 'keuntungan' => $keuntungan,
-                'laba_persen' => round($laba, 2)
+                'laba_persen' => round($laba, 2),
             ],
-            'kalangan' => $kalanganStats,
-            'iklan' => $iklanStats
+
+            'kalangan' => $kalangan,
+            'iklan' => $iklan,
+            'webinar' => $webinar,
+            'kalangan_terbanyak' => $kalanganTerbanyak
         ]);
     }
 }
